8224467063d9a36746f695dcb2ae8430887d6d3b
JPcGam3 <jaitniphat@gmail.com>
2025-06-23 17:13:43 +0700

    adjust: path for upload

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,11 @@ import {
   errorHandler,
 } from "./middlewares/error_handler.middleware";
 import cors from "cors";
-import path from "path";
-import fs from "fs";
 
 dotenv.config({ path: "../.env" });
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const uploadDir = path.join(__dirname, "..", "uploads", "images");
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 app.use(
   cors({
--- src/middlewares/image_upload.middleware.ts
+++ src/middlewares/image_upload.middleware.ts
@@ -3,14 +3,50 @@ import { Request } from "express";
 import path from "path";
 import fs from "fs";
 
-const uploadDir = path.join(__dirname, "../../uploads/evidence");
+const getWeekOfMonth = (date: Date): number => {
+  const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+  const dayOfMonth = date.getDate();
+
+  const firstDayOfWeek = firstDayOfMonth.getDay();
+
+  return Math.ceil((dayOfMonth + firstDayOfWeek) / 7);
+};
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+const weekOfMonth = getWeekOfMonth(currentDate);
+
+const monthDir = `${year}-${month}`;
+const weekDir = `week-${weekOfMonth}`;
+const uploadDir = path.join(
+  __dirname,
+  `../../uploads/images/evidence/${monthDir}/${weekDir}`
+);
+
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDir);
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const weekOfMonth = getWeekOfMonth(now);
+
+    const monthDir = `${year}-${month}`;
+    const weekDir = `week-${weekOfMonth}`;
+    const currentUploadDir = path.join(
+      __dirname,
+      `../../uploads/images/evidence/${monthDir}/${weekDir}`
+    );
+
+    if (!fs.existsSync(currentUploadDir)) {
+      fs.mkdirSync(currentUploadDir, { recursive: true });
+    }
+
+    cb(null, currentUploadDir);
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
