9d0201ae5cb0adf7d149213094513953e097d34e
JPcGam3 <jaitniphat@gmail.com>
2025-06-30 14:33:44 +0700

    fix: login middleware

--- src/routes/v1/mitr.route.ts
+++ src/routes/v1/mitr.route.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { MitrController } from "../../controllers/mitr.controller";
-import { AuthMiddleware } from "../../middlewares/current_user.middleware";
 
 const mitrRouter = Router();
 
-mitrRouter.get("/token", AuthMiddleware, MitrController.getToken);
-mitrRouter.post("/authen", AuthMiddleware, MitrController.getAuthen);
-mitrRouter.post("/profile", AuthMiddleware, MitrController.getProfile);
-mitrRouter.post("/profile/ad", AuthMiddleware, MitrController.getProfileAD);
-mitrRouter.post("/login", AuthMiddleware, MitrController.getLogin);
+mitrRouter.get("/token", MitrController.getToken);
+mitrRouter.post("/authen", MitrController.getAuthen);
+mitrRouter.post("/profile", MitrController.getProfile);
+mitrRouter.post("/profile/ad", MitrController.getProfileAD);
+mitrRouter.post("/login", MitrController.getLogin);
 
 export default mitrRouter;
